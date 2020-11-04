8f92f35cad7d8bad688097d0073b98bd2b5384d9
Will Gibbs <wtg17a@acu.edu>
2020-11-03 19:20:09 -0600

    Initial commit (part 1 complete)

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by William Gibbs on 11/3/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
